c9390e2eb6d15b742821a6a0086434e148003d5b
Narlotl <fretwellcalifornia@gmail.com>
2023-12-01 12:45:41 -0800

    Fix issues, update UI, add dark mode

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+document.body.setAttribute('data-bs-theme', localStorage.getItem('theme') || 'dark');
+document.getElementById('theme-icon').src = 'icons/' + (document.body.getAttribute('data-bs-theme') == 'light' ? 'moon' : 'sun') + '.svg';
+
 const params = new URLSearchParams(location.search);
 const page = params.get('page');
 const req = new XMLHttpRequest();
@@ -5,11 +8,11 @@ const req = new XMLHttpRequest();
 if (page == 'house' || page == 'senate') {
     document.getElementById('member-list').style.display = 'revert';
     document.querySelector('.active').classList.remove('active');
-    document.getElementById('nav-' + page).classList.add('active');
+    document.getElementById('nav-' + page).children[0].classList.add('active');
 
     const chamber = page == 'house' ? 'House of Representatives' : 'Senate';
     document.getElementById('jumbotron-title').innerHTML = 'U.S. ' + chamber;
-    document.title = (page == 'house' ? 'House of Representatives' : 'Senators') + ' - CHA';
+    document.title = (page == 'house' ? 'House of Representatives' : 'Senators') + ' - Civic Hacking Agency';
     let finishedLoading = false;
     let members = [];
     req.open('GET', /*'https://get.usa.govfresh.com/get?url=https://api.congress.gov/v3/member&limit=250&format=json'*/'https://api.congress.gov/v3/member?limit=250&format=json&api_key=fvA9u4qpBK9EY8vGLkPI55IDAh2qO0ImrORbmr0L');
@@ -39,10 +42,10 @@ if (page == 'house' || page == 'senate') {
                     name = name.slice(0, -1);
                 deck = document.getElementById('cards');
                 deck.innerHTML += `
-                    <div class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3 d-flex align-items-stretch">
+                    <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 d-flex align-items-stretch">
                         <div class="card clean">
                             <div class="card-body">
-                                <div class="fancy-2 ">
+                                <div class="fancy-image">
                                     <img src="${(member.depiction && member.depiction.imageUrl) ? member.depiction.imageUrl : 'person.png'}" class="md rounded-circle mb-3 ${member.partyName.toLowerCase().replace(' ', '-')}" alt="Headshot of ${name}">
                                 </div>
                                 <h3 class="h5">
@@ -69,53 +72,54 @@ else if (page == 'member') {
         document.head.querySelector('title').innerHTML = currentTerm.memberType + ' ' + data.directOrderName;
         document.getElementById('jumbotron-title').innerHTML = currentTerm.memberType + ' ' + data.directOrderName;
         document.getElementById('jumbotron-subtitle').innerHTML = `${data.state} (${data.partyHistory[data.partyHistory.length - 1].partyName})`;
-        document.querySelector('#member-data .col-sm-3').innerHTML = `
-             <div class="fancy-2"><img class="rounded-circle xl  mb-3 ${data.partyHistory[data.partyHistory.length - 1].partyName.toLowerCase().replace(' ', '-')}" alt="${data.directOrderName}" src="${data.depiction.imageUrl || '/assets/img/icons/1F9D1-200D-1F4BC.png'}"></div>
-             ${data.depiction.attribution ? `<p class="source">Photo source: ${data.depiction.attribution || ''}</p>` : ''}
-         `+ document.querySelector('#member-data .col-sm-3').innerHTML;
-        document.querySelector('#member-data .col-sm-6').innerHTML = `
-            <h2>Contact</h2>
+        document.querySelector('#member-data #member-info').innerHTML = `
+            <div class="fancy-image">
+                <img class="rounded-circle xl  mb-3 ${data.partyHistory[data.partyHistory.length - 1].partyName.toLowerCase().replace(' ', '-')}" alt="${data.directOrderName}" src="${data.depiction.imageUrl || '/assets/img/icons/1F9D1-200D-1F4BC.png'}">
+            </div>
+            <h3>Contact</h3>
             <ul>
             <li>Mail: ${data.addressInformation.officeAddress}</li>
             <li>Phone: <a href="tel:${data.addressInformation.phoneNumber}">${data.addressInformation.phoneNumber}</a></li>
-            <li>Website: <a href="${data.officialWebsiteUrl}">${new URL(data.officialWebsiteUrl).hostname}</a></li>
+            <li>Web: <a href="${data.officialWebsiteUrl}">${new URL(data.officialWebsiteUrl).hostname.replace('www.', '')}</a></li>
             </ul>
-            <h2>Terms</h2>
-            <ul class="terms"></ul>
-            <h2>Sponsored legislation</h2>
-            <p class="source" id="legislation-count"></p>
-            <ul class="legislation"></ul>
-        `+ document.querySelector('#member-data .col-sm-6').innerHTML;
-        data.terms.reverse().forEach(term => {
-            document.querySelector('ul.terms').innerHTML += `<li>${term.startYear}-${term.endYear || 'Present'} (${term.chamber})</li>`
-        });
+            <h3>Terms</h3>
+            <ul>
+                ${data.terms.reverse().map(term => `<li>${term.startYear}-${term.endYear || 'Present'} (${term.chamber})</li>`).join('\n')}
+            </ul>
+        `;
+        if (data.depiction && data.depiction.attribution)
+            document.getElementById('data-source').innerHTML += `<br>Image source: ${data.depiction.attribution}`;
 
         if (data.sponsoredLegislation) {
-            req.open('GET', 'https://get.usa.govfresh.com/get?url=' + data.sponsoredLegislation.url + '?format=json&limit=250&api_key=fvA9u4qpBK9EY8vGLkPI55IDAh2qO0ImrORbmr0L');
+            req.open('GET', /*'https://get.usa.govfresh.com/get?url=' +*/ data.sponsoredLegislation.url + '?format=json&limit=250&api_key=fvA9u4qpBK9EY8vGLkPI55IDAh2qO0ImrORbmr0L');
             req.onload = function () {
                 let legislation = JSON.parse(this.response).sponsoredLegislation.filter(piece => piece.type != null && piece.congress == currentTerm.congress);
                 document.getElementById('legislation-count').innerText = '(' + legislation.length + ' most recent in Congress ' + currentTerm.congress + ')';
                 for (let i = 0; i < legislation.length; i++)
                     legislation[i].introducedDate = new Date(legislation[i].introducedDate);
                 legislation.sort((a, b) => { return a.introducedDate.getTime() - b.introducedDate.getTime(); });
-                const ul = document.querySelector('ul.legislation');
-                if (legislation.length == 0)
-                    ul.innerHTML += '<li>' + data.directOrderName + ' has not sponsored any legislation yet.</li>';
+                const list = document.querySelector('#legislation');
                 let topics = new Map();
-                legislation.reverse().forEach(piece => {
-                    if (piece.policyArea.name)
-                        if (!topics.has(piece.policyArea.name))
-                            topics.set(piece.policyArea.name, 1);
-                        else
-                            topics.set(piece.policyArea.name, topics.get(piece.policyArea.name) + 1);
-                    ul.innerHTML += `
-                    <li><a target="_blank" href="https://congress.gov/bill/${currentTerm.congress}th-congress/${currentTerm.chamber.substring(0, (currentTerm.chamber.indexOf(' ') + 1 || currentTerm.chamber.length + 1) - 1).toLowerCase()}-${piece.type == 'S' || piece.type == 'HR' ? 'bill' : piece.type == 'SRES' || piece.type == 'HRES' ? 'resolution' : piece.type == 'SCONRES' ? 'concurrent-resolution' : 'joint-resolution'}/${piece.number}">${piece.title} (${piece.introducedDate.toLocaleDateString()})</a></li>
-                    `;
-                });
+                if (legislation.length == 0)
+                    list.innerHTML += '<p>' + data.directOrderName + ' has not sponsored any legislation yet.</p>';
+                else {
+                    legislation.reverse().forEach(piece => {
+                        if (piece.policyArea.name)
+                            if (!topics.has(piece.policyArea.name))
+                                topics.set(piece.policyArea.name, 1);
+                            else
+                                topics.set(piece.policyArea.name, topics.get(piece.policyArea.name) + 1);
+                        list.innerHTML += `
+                            <a class="list-group-item" target="_blank" href="https://congress.gov/bill/${currentTerm.congress}th-congress/${currentTerm.chamber.substring(0, (currentTerm.chamber.indexOf(' ') + 1 || currentTerm.chamber.length + 1) - 1).toLowerCase()}-${piece.type == 'S' || piece.type == 'HR' ? 'bill' : piece.type == 'SRES' || piece.type == 'HRES' ? 'resolution' : piece.type == 'SCONRES' ? 'concurrent-resolution' : 'joint-resolution'}/${piece.number}">
+                                ${piece.title} (${piece.introducedDate.toLocaleDateString()})
+                            </a>
+                        `;
+                    });
+                }
 
-                document.querySelector('.col-sm-6 img.loading').style.display = 'none';
+                document.querySelector('#sponsored-legislation img.loading').style.display = 'none';
 
-                req.open('GET', 'https://get.usa.govfresh.com/get?url=' + data.cosponsoredLegislation.url + '?format=json&limit=250&api_key=fvA9u4qpBK9EY8vGLkPI55IDAh2qO0ImrORbmr0L');
+                req.open('GET', /*'https://get.usa.govfresh.com/get?url=' + */data.cosponsoredLegislation.url + '?format=json&limit=250&api_key=fvA9u4qpBK9EY8vGLkPI55IDAh2qO0ImrORbmr0L');
                 req.onload = function () {
                     const cosponsoredLegislation = JSON.parse(this.response).cosponsoredLegislation.filter(piece => piece.type != null && piece.congress == currentTerm.congress);
                     for (const piece of cosponsoredLegislation)
@@ -125,21 +129,7 @@ else if (page == 'member') {
                             else
                                 topics.set(piece.policyArea.name, topics.get(piece.policyArea.name) + 1);
 
-                    /*const colors = [
-                        '#f2938c',
-                        '#f8e1de',
-                        '#f3bf90',
-                        '#dec69a',
-                        '#f5e6af',
-                        '#dfeacd',
-                        '#c7efe2',
-                        '#ccecf2',
-                        '#d9e8f6',
-                        '#e5e4fa',
-                        '#ebe3f9',
-                    ];*/
-                    document.querySelector('#member-data .col-sm-3#legislative-topics').innerHTML += '<h2>Legislative topics</h2><div id="topics"></div>';
-                    const topicsDiv = document.querySelector('#topics');
+                    const topicsDiv = document.getElementById('topics');
                     topics = [...topics.entries()].sort((a, b) => b[1] - a[1]);
                     const totalPieces = topics.reduce((a, b) => a + b[1], 0);
                     let randomIndex = parseInt(data.bioguideId.replaceAll(/[^0-9]/g, ''));
@@ -150,15 +140,15 @@ else if (page == 'member') {
                             hsl[i] = random - Math.floor(random);
                             randomIndex++;
                         }
-                        // TODO: box is too big
-                        // TODO: Sanders's
                         // TODO: smallest box should be 1rem + padding
-                        const color = `hsl(${Math.round(hsl[0] * 360)}, ${Math.round(hsl[1] * 30) + 20}%, ${Math.round(hsl[2] * 30) + 20}%)`;
                         const percent = Math.round(100 * topic[1] / totalPieces);
-                        topicsDiv.innerHTML += `<div class="topic" style="background-color: ${color}; max-height: ${window.innerHeight * topic[1] / totalPieces}px; min-height: ${window.innerHeight * topic[1] / totalPieces}px"><span>${topic[0]} (${percent >= 1 ? percent : '<1'}%)</span></div>`;
+                        topicsDiv.innerHTML += `
+                            <div class="topic" style="--topic-dark: hsl(${Math.round(hsl[0] * 360)}, ${Math.round(hsl[1] * 30) + 20}%, ${Math.round(hsl[2] * 30) + 20}%); --topic-light: hsl(${Math.round(hsl[0] * 360)}, ${Math.round(hsl[1] * 30) + 60}%, ${Math.round(hsl[2] * 30) + 60}%); max-height: ${window.innerHeight * topic[1] / totalPieces}px; min-height: ${window.innerHeight * topic[1] / totalPieces}px">
+                                <span>${topic[0]} (${percent >= 1 ? percent : '<1'}%)</span>
+                            </div>`;
                     });
 
-                    document.querySelector('.col-sm-3 img.loading').style.display = 'none';
+                    document.querySelector('#legislative-topics img.loading').style.display = 'none';
                 };
                 req.send();
             };
@@ -169,6 +159,12 @@ else if (page == 'member') {
 }
 else {
     document.getElementById('home').style.display = 'revert';
-    document.querySelector('.nav-item.active').classList.remove('active');
+    document.querySelector('.active').classList.remove('active');
     document.getElementById('nav-home').classList.add('active');
+    fetch('https://api.congress.gov/v3/congress?api_key=fvA9u4qpBK9EY8vGLkPI55IDAh2qO0ImrORbmr0L').then(res => res.json()).then(data => {
+        const current = data.congresses[0].name.substring(0, 3);
+        fetch('https://api.congress.gov/v3/committee/' + current + '?api_key=fvA9u4qpBK9EY8vGLkPI55IDAh2qO0ImrORbmr0L&limit=5000').then(res => res.json()).then(data => {
+            console.log(data);
+        });
+    });
 }
